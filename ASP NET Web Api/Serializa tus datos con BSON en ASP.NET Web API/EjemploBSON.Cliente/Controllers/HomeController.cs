fd8e5666e090595b3b95200b697a049a403b79bc
Hernan Guzman <hernandgr@gmail.com>
2014-07-25 21:00:13 -0500

    Actualización de documentación.

--- ASP NET Web Api/Serializa tus datos con BSON en ASP.NET Web API/EjemploBSON.Cliente/Controllers/HomeController.cs
+++ ASP NET Web Api/Serializa tus datos con BSON en ASP.NET Web API/EjemploBSON.Cliente/Controllers/HomeController.cs
@@ -13,8 +13,10 @@ namespace EjemploBSON.Cliente.Controllers
     {
         public ActionResult Index()
         {
+            // Obtener lista de personas.
             List<Persona> personas = Task.Run(async () => await ObtenerPersonas()).Result;
 
+            // Envío de datos a la vista.
             return View(personas);
         }
 
@@ -22,19 +24,22 @@ namespace EjemploBSON.Cliente.Controllers
         {
             List<Persona> personas = null;
 
+            // Creación de instancia de HttpClient para acceder a servicio Web API.
             using (HttpClient cliente = new HttpClient())
             {
+                // URL donde se encuentra alojado el servicio Web API.
                 cliente.BaseAddress = new Uri("http://localhost:60461");
 
-                // Adicionar encabezado con el tipo de contenido (application/bson)
+                // Adicionar encabezado con el tipo de contenido (application/bson).
+                // Con esta instrucción, el servicio Web API determina la serialización a usar.
                 cliente.DefaultRequestHeaders.Accept.Clear();
                 cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/bson"));
 
-                // Enviar petición al servicio
+                // Enviar petición al servicio.
                 var resultado = await cliente.GetAsync("api/personas");
                 resultado.EnsureSuccessStatusCode();
 
-                // Deserializar el resultado
+                // Deserializar el resultado.
                 MediaTypeFormatter[] formatos = new MediaTypeFormatter[] { new BsonMediaTypeFormatter() };
                 personas = await resultado.Content.ReadAsAsync<List<Persona>>(formatos);
             }
