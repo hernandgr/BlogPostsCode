3aafea2c7ed64eaecaf8499920400e42d5b87def
Hernan Guzman <hernandgr@gmail.com>
2014-11-15 18:35:30 -0500

    Adición de ejemplo con ELMAH.

---
+++ ASP NET MVC/Manejo de Excepciones con ELMAH/ELMAHTest.Web/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ELMAHTest.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            throw new Exception("Oops!... better check ELMAH");
+        }
+    }
+}
---
+++ ASP NET MVC/Manejo de Excepciones con ELMAH/ELMAHTest.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ELMAHTest.Web.MvcApplication" Language="C#" %>
