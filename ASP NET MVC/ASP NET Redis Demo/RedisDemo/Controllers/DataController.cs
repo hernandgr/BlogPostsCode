116d13d671d9fba0912bcc2fa1f5c24b92084476
Hernan Guzman Rendon <hernandgr@gmail.com>
2014-12-20 20:33:23 -0500

    Adición de ejemplo de conexión a Redis.

---
+++ ASP NET MVC/ASP NET Redis Demo/RedisDemo/Controllers/DataController.cs
@@ -0,0 +1,64 @@
+﻿using RedisDemo.Models;
+using StackExchange.Redis;
+using System.Web.Mvc;
+
+namespace RedisDemo.Controllers
+{
+    public class DataController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Data data)
+        {
+            ModelState.Remove("Value");
+            data.Message = string.Empty;
+            data.Value = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+                var db = redis.GetDatabase();
+
+                var value = db.StringGet(data.Key);
+
+                if (value.HasValue)
+                {
+                    data.Value = value;
+                }
+                else
+                {
+                    data.Message = "Key not found.";
+                }
+            }
+
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Data data)
+        {
+            if (ModelState.IsValid)
+            {
+                string server = "localhost";
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(server);
+                var db = redis.GetDatabase();
+
+                db.StringSet(data.Key, data.Value);
+
+                return RedirectToAction("Create");
+            }
+
+            return View(data);
+        }
+    }
+}
---
+++ ASP NET MVC/ASP NET Redis Demo/RedisDemo/Models/Data.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RedisDemo.Models
+{
+    public class Data
+    {
+        [Required]
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public string Message { get; set; }
+    }
+}
