45102143012c3120e68a484c30c9f2ea7631c4e6
Hernan Guzman Rendon <hernandgr@gmail.com>
2015-09-18 19:37:19 -0500

    Ejemplo de descarga de archivos.

---
+++ ASP NET MVC/Descarga de archivos en ASP.NET MVC/MvcDownloadFile/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MvcDownloadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult Download()
+        {
+            // Obtener contenido del archivo
+            string text = "El texto para mi archivo.";
+
+
+            return File(Encoding.ASCII.GetBytes(text), "text/plain", "Prueba.txt");
+        }
+    }
+}
